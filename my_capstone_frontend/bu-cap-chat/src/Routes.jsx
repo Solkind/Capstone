dafd6acb250eddbcc8940f7111f5d855a1e7f1db
Solkind <119994828+Solkind@users.noreply.github.com>
2023-06-24 22:11:44 -0400

    chat app draft

--- my_capstone_frontend/bu-cap-chat/src/Routes.jsx
+++ my_capstone_frontend/bu-cap-chat/src/Routes.jsx
@@ -1,17 +1,14 @@
+import RegisterAndLoginForm from "./RegisterAndLoginForm";
 import { useContext } from "react";
 import { UserContext } from "./UserContext";
-import RegisterAndLoginForm from "./RegisterAndLoginForm";
 import Chat from "./Chat";
 
 export default function Routes() {
-    const {username, id} = useContext(UserContext);
+  const { username, id } = useContext(UserContext);
 
-    if (username) {
-        return <Chat />;
-    }
+  if (username) {
+    return <Chat />;
+  }
 
-
-    return (
-        <RegisterAndLoginForm />
-    );
+  return <RegisterAndLoginForm />;
 }
